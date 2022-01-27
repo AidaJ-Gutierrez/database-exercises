dd30c316ab8fa34c7c66642fa6a1248d57e4d3d3
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-01-27 14:36:40 -0600

    databeases function update

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,7 +2,7 @@ USE employees;
 
 # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 
-SELECT CONCAT(first_name, last_name)
+SELECT CONCAT(first_name, ' ',last_name)
 FROM employees
 WHERE last_name LIKE 'E%E';
 
@@ -42,3 +42,11 @@ FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
   AND (birth_date LIKE '%-12-25')
 ORDER By 'Days in Company';
+
+# or :
+
+SELECT first_name, last_name, CONCAT(datediff(now(),hire_date), ' days' )
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND (birth_date LIKE '%-12-25')
+ORDER BY hire_date DESC;
